24d29913d9fb588c61f2eaf0f4f7555c32ed983e
WaydeBrown <brown.wayde@gmail.com>
2016-03-06 14:40:54 +0100

    test image
    image to test that open cv was working

--- streamVideo.py
+++ streamVideo.py
@@ -1,52 +1,19 @@
-import cv2 as cv
+
+import cv2
 import urllib
 import numpy as np
 
 
-vcap = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream")
+
+vcap = cv2.VideoCapture('rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream')
+
 while(1):
+    #print vcap.isOpened()
     ret, frame = vcap.read()
-    print vcap.get(4)
-    print vcap.get(3)
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) & 0xFF ==ord('q'):
+    #print vcap.get(4)
+    #print vcap.get(3)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
         break
 
-"""
-
-CvCapture *camera = cvCreateFileCapture("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov");
-if (camera == NULL) {
- printf("video is null, aborting...");
- return -1;
-}
-else{
- printf("video ok");
-}
-
-cv::VideoCapture vcap;
-//open the video stream and make sure it's opened
-if(!vcap.open("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov")) {
-    std::cout << "Error opening video stream or file" << std::endl;
-    return -1;
-}
-
-cv::VideoCapture capture("rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream");
-
-if (!capture->isOpened()) {
-    //Error
-}
-
-
-cv::namedWindow("TEST", CV_WINDOW_AUTOSIZE);
-
-cv::Mat frame;
-
-while(m_enable) {
-    if (!capture->read(frame)) {
-        //Error
-    }
-    cv::imshow("TEST", frame);
-
-    cv::waitKey(30);
-}
-"""
