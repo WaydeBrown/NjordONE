d78627ac4a903f8e068b851453258af7e3fd620e
WaydeBrown <brown.wayde@gmail.com>
2016-03-06 15:13:49 +0100

    Working release. Import and show video
    can load and show one video feed from IP camera

--- streamVideo.py
+++ streamVideo.py
@@ -3,9 +3,10 @@ import cv2
 import urllib
 import numpy as np
 
+stream1Url = 'rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream'
+stream2Url = 'rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream'
 
-
-vcap = cv2.VideoCapture('rtsp://192.168.1.10:554/user=admin_password=d2gNs1nj_channel=1_stream=0.sdp?real_stream')
+vcap = cv2.VideoCapture(stream1Url)
 
 while(1):
     #print vcap.isOpened()
